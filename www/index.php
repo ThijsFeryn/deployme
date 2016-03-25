5b13386fae13f0be2c752fda7b5e82af29bf908b
ThijsFeryn <thijs.feryn@combellgroup.com>
2016-03-25 09:43:04 +0100

    Adding Silex dependency

--- www/index.php
+++ www/index.php
@@ -1,2 +1,14 @@
 <?php
-echo "Hello from release ".basename(__DIR__).PHP_EOL;
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/hello/{name}', function($name) use($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->get('/', function()  {
+    return "Hello from release ".basename(__DIR__).PHP_EOL;
+});
+
+$app->run();
